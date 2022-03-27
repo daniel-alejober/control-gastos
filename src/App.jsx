a92bf365d4ce34ac76fffa81e33a2273910d5ec3
unknown <daniel-alejo94@hotmail.com>
2022-03-26 19:08:49 -0600

    agregando local storage

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,52 @@ import ListadoGastos from "./components/ListadoGastos";
 import { generarId } from "./helpers/generarId";
 import IconoNuevoGasto from "./img/nuevo-gasto.svg";
 function App() {
-  const [presupuesto, setPresupuesto] = useState(0);
+  /*vamos a cambiar el valor inicial ya que si hay un presupuesto en LS tomara ese presupuesto
+  y si no hay ninguno sera 0 */
+  const [presupuesto, setPresupuesto] = useState(
+    Number(localStorage.getItem("presupuesto")) ?? 0
+  );
+  /*Su valor inicial sera se encuentra algo en LS enonces tomara ese valor
+  y si no sera un array vacio */
+  const [gastos, setGastos] = useState(
+    localStorage.getItem("gastos")
+      ? JSON.parse(localStorage.getItem("gastos"))
+      : []
+  );
   const [presupuestoValido, setPresupuestoValido] = useState(false);
   const [ventanaModal, setVentanaModal] = useState(false);
   const [animarModal, setAnimarModal] = useState(false);
-  const [gastos, setGastos] = useState([]);
   const [editarGasto, setEditarGasto] = useState({});
 
+  /*abrira la ventana modal si hay valores en editar gasto */
+  useEffect(() => {
+    if (Object.keys(editarGasto).length > 0) {
+      setVentanaModal(true);
+      setTimeout(() => {
+        setAnimarModal(true);
+      }, 600);
+    }
+  }, [editarGasto]);
+
+  /*Guardar el LS, le ponemos un nombre y su valor, y si no esta presente ese valor
+  le vamos a poner un 0*/
+  useEffect(() => {
+    localStorage.setItem("presupuesto", presupuesto ?? 0);
+  }, [presupuesto]);
+
+  /*ya que no podemos almacenar arreglos en LS lo convertimos a string */
+  useEffect(() => {
+    localStorage.setItem("gastos", JSON.stringify(gastos) ?? []);
+  }, [gastos]);
+
+  /*este usf se encarga de verificar si ya hay un presupuesto en ls si lo hay
+  va a redireccionar al dashboard*/
+  useEffect(() => {
+    const presupuestoLS = Number(localStorage.getItem("presupuesto")) ?? 0;
+    if (presupuestoLS > 0) {
+      setPresupuestoValido(true);
+    }
+  }, []);
   /*Esta funcion se encargar de mostrar la ventana modal */
   const handleNuevoGasto = () => {
     setVentanaModal(true);
@@ -37,15 +76,6 @@ function App() {
       setGastos([...gastos, gasto]);
     }
   };
-  /*abrira la ventana modal si hay valores en editar gasto */
-  useEffect(() => {
-    if (Object.keys(editarGasto).length > 0) {
-      setVentanaModal(true);
-      setTimeout(() => {
-        setAnimarModal(true);
-      }, 600);
-    }
-  }, [editarGasto]);
 
   const eliminarGasto = (id) => {
     const gastoEliminado = gastos.filter((gasto) => gasto.id !== id);
